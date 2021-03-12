bd1ead9c0f7616bc06506a0da98763c3ff394833
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-03-12 14:23:14 -0500

    Moved API queries to App.ja

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,51 +4,29 @@ import { IoLocationSharp } from "react-icons/io5";
 import Loading from "./Loading";
 import FloatingInfo from "./FloatingInfo";
 
-export default function Banner() {
-    const [data, setData] = useState(null);
+export default function Banner(props) {
+    const data = props.current;
 
-    useEffect(() => {
-        // Get JSON data using Ajax
-        const Http = new XMLHttpRequest();
-        // Bogotá's id
-        const city = "3688689";
-        const key = process.env.REACT_APP_OWM_KEY;
-        const url = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${key}&units=metric`;
-
-        Http.open("GET", url);
-        Http.send();
-
-        Http.onreadystatechange = e => {
-            const { readyState, status } = Http;
-
-            if (readyState === 4 && status === 200) {
-                // Simulate server wait time to show loader
-                setTimeout(() => {
-                    setData(JSON.parse(Http.responseText));
-                    console.log(data);
-                }, 1500);
-            }
-        };
-    }, []);
-
-    if (data) {
-        let info = {
-            weather: data.weather,
-            main: data.main
-        };
-
-        return (
-            <div>
-                <div className='banner'>
-                    <div className='name text-primary'>
-                        <IoLocationSharp className='icon' />
-                        <div>{data.name}</div>
-                    </div>
-                </div>
-                <FloatingInfo info={info} />
-            </div>
-        );
+    if (!data) {
+        return <Loading />;
     }
 
-    return <Loading />;
+    let info = {
+        weather: data.weather,
+        main: {
+            temp: data.temp
+        }
+    };
+
+    return (
+        <div>
+            <div className='banner'>
+                <div className='name text-primary'>
+                    <IoLocationSharp className='icon' />
+                    <div>Bogotá</div>
+                </div>
+            </div>
+            <FloatingInfo info={info} />
+        </div>
+    );
 }
