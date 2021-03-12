e7915e1a9585a229bd7fc20beb77951612430c16
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-03-12 14:26:47 -0500

    Passes data along

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-import Cards from "./Cards";
+import CityCards from "./CityCards";
 import Forecast from "./Forecast";
 import Places from "./Places";
 
-export default function Sections() {
+export default function Sections(props) {
+    const { data } = props;
+
     return (
         <div className='sectionsWrapper'>
-            <Forecast />
+            <Forecast data={data.slice(1, 4)} />
             <Places />
-            <Cards />
+            <CityCards />
         </div>
     );
 }
