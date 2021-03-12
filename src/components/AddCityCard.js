52b4f2b1d29c21145fd4950398e6eaf078cc5825
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-03-12 16:09:29 -0500

    Card for adding cities

---
+++ src/components/AddCityCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddCityCard() {
+    return (
+        <div className='addCity'>
+            <button>Add Location</button>
+        </div>
+    );
+}
