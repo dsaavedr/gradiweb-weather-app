4a6cb3ccefbfceb5b7e388a940196fe947897cad
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-03-12 14:26:26 -0500

    Minor change

--- src/components/Places.js
+++ src/components/Places.js
@@ -4,7 +4,7 @@ export default function Places() {
     return (
         <div className='places'>
             <h2>
-                <strong>Places to</strong> visit
+                <strong>Places to</strong> Visit
             </h2>
         </div>
     );
